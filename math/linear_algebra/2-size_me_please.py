0b8862729e26aa4f13ffb70eddf6ff4619887b47
Ziga Larissa <ziga0902larissa@gmail.com>
2024-01-11 03:49:47 -0800

    Size Me Please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+#def the function
 def matrix_shape(matrix):
     shape = []
+    #check if the mmatrix is a list
     while isinstance(matrix, list):
         shape.append(len(matrix))
         matrix = matrix[0] if matrix else None
