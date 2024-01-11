92142e8cb507773dc5c38333a4618c0afca07052
Ziga Larissa <ziga0902larissa@gmail.com>
2024-01-11 03:55:17 -0800

    Size Me Please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -4,6 +4,8 @@ def matrix_shape(matrix):
     shape = []
     # check if the mmatrix is a list
     while isinstance(matrix, list):
+        # if a list append its length to shape
         shape.append(len(matrix))
+        # change the matrix to the first index of what was matrix before
         matrix = matrix[0] if matrix else None
     return shape
