d65bfcfca42269f9621576cf911b9d25c60affb7
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-01-14 09:15:19 +0200

    returns new mat

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -10,7 +10,12 @@ def add_matrices2D(mat1, mat2):
     mat2: list of lists of ints/floats
     """
     if len(mat1) == len(mat2) or len(mat1[0]) == len(mat2[0]):
-        return [mat1[i][j] + mat2[i][j] for i in range(len(mat1))
-            for j in range(len(mat1[0]))]
+        return [
+            [
+                mat1[i][j] + mat2[i][j]
+                for j in range(len(mat1[0]))
+            ]
+            for i in range(len(mat1))
+        ]
     else:
         return None
