9f3bec0e85318496da2844ef240423b1bcb813ec
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-01-14 08:31:33 +0200

    pycodestyle validation

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -9,5 +9,5 @@ def matrix_transpose(matrix):
     matrix: matrix to be transposed
     """
     trans_matrix = [[matrix[j][i] for j in range(len(matrix))]
-                            for i in range(len(matrix[0]))]
+                    for i in range(len(matrix[0]))]
     return trans_matrix
