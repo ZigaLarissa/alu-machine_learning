7a9ae3df41812c40db67c6f3dee87535d7476991
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-01-14 07:54:37 +0200

    Size Me Please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-""" counts the shape of the matrix regardless of the dimensions."""
+""" 
+   counts the shape of the matrix regardless of the dimensions.
+"""
 
 
 def matrix_shape(matrix):
+    """
+    matrix: matrix to be evaluated
+    """
     shape = []
-
-    # check if the mmatrix is a list
     while isinstance(matrix, list):
         shape.append(len(matrix))
         matrix = matrix[0] if matrix else None
