53f56f687b793c1b5be4e1b23b98e013739806d7
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-01-31 14:35:41 +0200

    returns None on an invalid poly

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -9,7 +9,9 @@ def poly_derivative(poly):
     if poly not valid return None, and
     if derivative is 0 return [0]
     """
-    if not isinstance(poly, list) or len(poly) <= 1:
+    if not isinstance(poly, list):
+        return None
+    elif len(poly) <= 1:
         return [0]
     deriv_poly = [poly[i] * i for i in range(1, len(poly))]
     return deriv_poly
