103feb711af95b8a24f2e43e663b2dbc31ff55b2
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-01-31 14:38:25 +0200

    returns none if poly empty

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -9,7 +9,7 @@ def poly_derivative(poly):
     if poly not valid return None, and
     if derivative is 0 return [0]
     """
-    if not isinstance(poly, list):
+    if not isinstance(poly, list) or len(poly) == 0:
         return None
     elif len(poly) <= 1:
         return [0]
