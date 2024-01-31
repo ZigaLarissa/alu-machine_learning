8eee0c5ef0f859c0a10e18e3245fc71a0a6199ef
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-01-31 14:08:27 +0200

    sum of squares

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""
+calculates the sum of the first n**2 natural numbers
+"""
+
+
+def summation_i_squared(n):
+    sum_of_squares = (n * (n + 1) * ((2 * n) + 1)) // 6
+    return sum_of_squares
