97c90cac653a404030f3d68b203afb5d81a3f644
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-02-09 02:40:53 +0200

    z_score and x_value functions

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -30,3 +30,15 @@ class Normal():
             for i in data:
                 summation += (i - self.mean) ** 2
             self.stddev = (summation / len(data)) ** 0.5
+
+    def z_score(self, x):
+        """
+        Calculates the z-score of a given x-value
+        """
+        return ((x - self.mean) / self.stddev)
+
+    def x_value(self, z):
+        """
+        Calculates the x-score of a given z-value
+        """
+        return ((z * self.stddev) + self.mean)
