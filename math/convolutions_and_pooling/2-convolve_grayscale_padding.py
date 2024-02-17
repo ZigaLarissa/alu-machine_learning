1f8ced9b80e3ab2ca7009b125f2d75710cc9614b
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-02-17 16:55:32 +0200

    padding

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -23,13 +23,25 @@ def convolve_grayscale_padding(images, kernel, padding):
                 pw is the padding for the width of the image
     Returns: numpy.ndarray containing the convolved images
     """
-    m, h, w = images.shape
-    kh, kw = kernel.shape
-    ph, pw = padding
-    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
-    conv = np.zeros((m, h, w))
-    for i in range(h):
-        for j in range(w):
-            image = padded[:, i:i+kh, j:j+kw]
-            conv[:, i, j] = np.sum(image * kernel, axis=(1, 2))
-    return conv
+    m = images.shape[0]
+    height = images.shape[1]
+    width = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    if kh % 2 == 1:
+        ph = (kh - 1) // 2
+    else:
+        ph = kh // 2
+    if kw % 2 == 1:
+        pw = (kw - 1) // 2
+    else:
+        pw = kw // 2
+    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
+                    'constant', constant_values=0)
+    convoluted = np.zeros((m, height, width))
+    for h in range(height):
+        for w in range(width):
+            output = np.sum(images[:, h: h + kh, w: w + kw] * kernel,
+                            axis=1).sum(axis=1)
+            convoluted[:, h, w] = output
+    return convoluted
