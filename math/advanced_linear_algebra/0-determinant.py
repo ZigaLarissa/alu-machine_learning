cb2c4d13af591ee0f8518b9de3813527600c48c2
Ziga Larissa <l.bizimungu@alustudent.com>
2024-02-22 13:11:33 +0200

    determinant

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -15,4 +15,15 @@ def determinant(matrix):
         raise TypeError("matrix must be a list of lists")
     if not all(len(matrix) == len(row) for row in matrix):
         raise ValueError("matrix must be a square matrix")
-    return np.linalg.det(matrix)
+    
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    
+    det = 0
+    for j in matrix[0]:
+        minor = [row[1:] for row in matrix[1:]]
+        minor = [minor[i][:j] + minor[i][j+1:] for i in range(len(minor))]
+        det += j * (-1) ** j * determinant(minor)
+    return det
