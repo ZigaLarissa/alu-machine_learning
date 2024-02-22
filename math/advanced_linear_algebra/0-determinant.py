c8b5d7680d4db33da024dfecf0a9e1585e9f2c46
Ziga Larissa <l.bizimungu@alustudent.com>
2024-02-22 14:13:01 +0200

    matrix size

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -3,31 +3,30 @@
 This module contains the function to calculate the determinant of a matrix.
 """
 
-def smaller_matrix(matrix, i, j):
-    """
-    Returns the matrix with the i-th row and j-th column removed.
-    """
-    return [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
 
 def determinant(matrix):
     """
     Calculates the determinant of a matrix.
     """
-    if not isinstance(matrix, list) and not all(isinstance(row, list) for row in matrix):
+    if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
-    if len(matrix) == 0 and len(matrix) == 1 and not all(len(matrix) == len(row) for row in matrix):
+        
+    if not all(len(matrix) == len(row) for row in matrix):
         raise ValueError("matrix must be a square matrix")
-    
+        
     if len(matrix) == 0:
-        return 1
-
+        return 0
+    
     if len(matrix) == 1:
         return matrix[0][0]
     
     if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    
+
+    size = len(matrix)
     answer = 0
-    for j in range(matrix):
-        answer += ((-1) ** j) * matrix[0][j] * determinant(smaller_matrix(matrix, 0, j))
+    for j in range(size):
+        sign = (-1)**j
+        sub_matrix = [row[:j] + row[j+1:] for row in matrix[1:]]
+        answer += sign * matrix[0][j] * determinant(sub_matrix)
     return answer
