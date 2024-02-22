c10e175265c05ea8af2f8ddb73913ea7423a3c7b
Ziga Larissa <l.bizimungu@alustudent.com>
2024-02-22 13:51:25 +0200

    recursive

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -13,14 +13,16 @@ def determinant(matrix):
     if not all(len(matrix) == len(row) for row in matrix):
         raise ValueError("matrix must be a square matrix")
     
+    if len(matrix) == 0:
+        return 1
+
     if len(matrix) == 1:
         return matrix[0][0]
+    
     if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     
-    det = 0
-    for j in matrix[0]:
-        minor = [row[1:] for row in matrix[1:]]
-        minor = [minor[i][:j] + minor[i][j+1:] for i in range(len(minor))]
-        det += j * (-1) ** j * determinant(minor)
-    return det
+    answer = 0
+    for j in range(matrix):
+        answer += ((-1) ** j) * matrix[0][j] * determinant([[matrix[i][j] for j in range(1, len(matrix))] for i in range(1, len(matrix))])
+    return answer
