07241c9169b225fca59ceac21996b6e4959f63bf
Ziga Larissa <l.bizimungu@alustudent.com>
2024-02-22 14:20:25 +0200

    matrix 0*0

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -8,11 +8,11 @@ def determinant(matrix):
     """
     Calculates the determinant of a matrix.
     """
-    if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a list of lists")
-
     if len(matrix) == 0:
         return 1
+
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
 
     if not all(len(matrix) == len(row) for row in matrix):
         raise ValueError("matrix must be a square matrix")
