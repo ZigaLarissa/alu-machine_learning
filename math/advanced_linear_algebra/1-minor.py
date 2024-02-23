8f8c0e1979de680f617f2757726d6f1edadf61cd
Ziga Larissa <l.bizimungu@alustudent.com>
2024-02-23 12:36:07 +0200

    range now iteratable

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -48,7 +48,7 @@ def minor(matrix):
     minor_matrix = []
     for i in range(len(matrix)):
         minor_row = []
-        for j in len(matrix):
+        for j in range(len(matrix)):
             sub_matrix = [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
             minor_row.append(determinant(sub_matrix))
         minor_matrix.append(minor_row)
