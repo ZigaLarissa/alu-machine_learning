d5485e2e7849452ea008a802f40d9f62c9d630de
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-02-24 21:10:19 +0200

    defineteness of a matrix

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -5,68 +5,26 @@ This module contains the function to calculate the definiteness of a matrix.
 import numpy as np
 
 
-def is_positive_definite(matrix):
+def definiteness(matrix):
     """
-    defines if a matrix is positive definite
+    calculates the definiteness of a matrix
     """
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
-    if matrix.shape[0] != matrix.shape[1]:
-        return False
+
+    if not np.array_equal(matrix, matrix.T):
+        return None
+
     if not np.all(np.linalg.eigvals(matrix) > 0):
-        return False
-    return True
+        return "Positive definite"
 
-
-def is_positive_semidefinite(matrix):
-    """
-    defines if a matrix is positive semidefinite
-    """
-    if not isinstance(matrix, np.ndarray):
-        raise TypeError("matrix must be a numpy.ndarray")
-    if len(matrix.shape) != 2:
-        return False
-    if matrix.shape[0] != matrix.shape[1]:
-        return False
     if not np.all(np.linalg.eigvals(matrix) >= 0):
-        return False
-    return True
+        return "Positive semi-definite"
 
-
-def is_negative_definite(matrix):
-    """
-    defines if a matrix is negative definite
-    """
-    if not isinstance(matrix, np.ndarray):
-        raise TypeError("matrix must be a numpy.ndarray")
-    if matrix.shape[0] != matrix.shape[1]:
-        return False
     if not np.all(np.linalg.eigvals(matrix) < 0):
-        return False
-    return True
+        return "Negative definite"
 
-
-def is_negative_semidefinite(matrix):
-    """
-    defines if a matrix is negative semidefinite
-    """
-    if not isinstance(matrix, np.ndarray):
-        raise TypeError("matrix must be a numpy.ndarray")
-    if matrix.shape[0] != matrix.shape[1]:
-        return False
     if not np.all(np.linalg.eigvals(matrix) <= 0):
-        return False
-    return True
+        return "Negative semi-definite"
 
-
-def is_indefinite(matrix):
-    """
-    defines if a matrix is indefinite
-    """
-    if not isinstance(matrix, np.ndarray):
-        raise TypeError("matrix must be a numpy.ndarray")
-    if matrix.shape[0] != matrix.shape[1]:
-        return False
-    if not any(np.linalg.eigvals(matrix) > 0) and not any(np.linalg.eigvals(matrix) < 0):
-        return False
-    return True
+    return "Indefinite"
