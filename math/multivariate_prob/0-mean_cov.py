c1e677dad283348d4be827a02d5ccffac30f8399
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-03-12 01:01:08 +0200

    mean and covariance

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -7,13 +7,25 @@ This module contains the function to calculate the mean and covariance of a mult
 
 def mean_cov(X):
     """
-    This function calculates the mean and covariance of
-    a multivariate distribution.
+    args:
+    - X: numpy.ndarray (d, n) containing the data set:
+        - d: number of dimensions
+        - n: number of data points
+
+    returns:
+    mean, cov:
+    - mean: numpy.ndarray (d, 1) containing the mean of the data set
+    - cov: numpy.ndarray (d, d) containing the covariance matrix of the data set
     """
-    if type(X) is not np.ndarray:
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
-    if n < 2:
-        raise ValueError("X must contain multiple data points")
-    mean = np.mean(X, axis=0)
-    cov = np.dot((X - mean).T, (X - mean)) / (X.shape[0] - 1)
+    
+    if X.shape[0] < 2:
+        raise ValueError('X must contain multiple data points')
+    
+    n, d = X.shape
+    mean = np.mean(X, axis=0, keepdims=True)
+    center_data = X - mean
+    len_data = X.shape[0] - 1
+    cov = np.dot(center_data.T, center_data) / len_data
     return mean, cov
