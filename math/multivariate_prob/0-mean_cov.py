31c7d790813f402a6f47feeafa546f462f316c33
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-03-12 00:49:36 +0200

    confirm 2d, and contains multiple data

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -10,6 +10,10 @@ def mean_cov(X):
     This function calculates the mean and covariance of
     a multivariate distribution.
     """
+    if type(X) is not np.ndarray:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
     mean = np.mean(X, axis=0)
-    cov = np.dot((X - mean).T, (X - mean)) / (X.shape[0])
+    cov = np.dot((X - mean).T, (X - mean)) / (X.shape[0] - 1)
     return mean, cov
