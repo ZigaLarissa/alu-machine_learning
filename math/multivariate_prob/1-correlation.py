06ab1ac4b504fe3ae1ab25e30388ba1410760d49
Ziga Larissa <l.bizimungu@alustudent.com>
2024-03-12 16:20:41 +0200

    2D Validation

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -21,9 +21,8 @@ def correlation(C):
      A numpy.ndarray of shape (d, d) containing the
         correlation matrix.
     """
-    d, d = C.shape
     if type(C) is not np.ndarray:
         raise TypeError("C must be a numpy.ndarray")
-    if len(C.shape) != 2 and d != 2: 
+    if len(C.shape) != 2 and C[0] != 2:
         raise ValueError("C must be a 2D square matrix")
     return np.corrcoef(C)
