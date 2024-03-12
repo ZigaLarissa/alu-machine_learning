343e4c0aecfe65f4d90fb84abef3e2f1737c7744
Ziga Larissa <l.bizimungu@alustudent.com>
2024-03-12 16:15:09 +0200

    check if it is a 2d matrix

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -23,6 +23,6 @@ def correlation(C):
     """
     if type(C) is not np.ndarray:
         raise TypeError("C must be a numpy.ndarray")
-    if len(C.shape) != 2:
+    if len(C.shape) != 2 and C.shape[0] != C.shape[1]: 
         raise ValueError("C must be a 2D square matrix")
     return np.corrcoef(C)
