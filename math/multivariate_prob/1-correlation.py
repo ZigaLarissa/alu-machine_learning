1010540909e78f5509f3ba36e352f76abd37b663
Ziga Larissa <l.bizimungu@alustudent.com>
2024-03-12 16:44:50 +0200

    corrcoef

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -23,6 +23,6 @@ def correlation(C):
     """
     if type(C) is not np.ndarray:
         raise TypeError("C must be a numpy.ndarray")
-    if len(C.shape) != 2 and C[0] != 2:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]: 
         raise ValueError("C must be a 2D square matrix")
-    return np.corrcoef(C)
+    return np.corrcoef(C, rowvar=False)
