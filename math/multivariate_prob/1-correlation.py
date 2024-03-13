0749116484802d064da3673179a187ad13a7d827
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-03-13 21:21:01 +0200

    pycodestyle

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -24,7 +24,7 @@ def correlation(C):
     """
     if type(C) is not np.ndarray:
         raise TypeError("C must be a numpy.ndarray")
-    if len(C.shape) != 2 or C.shape[0] != C.shape[1]: 
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
 
     d = C.shape[0]
