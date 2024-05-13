cceff6673e07473d217767b15e8ff02a562a731c
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-05-13 07:34:57 +0200

    NeuralNetwork class initialization

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3v
+"""
+This module contains the NeuralNetwork class.
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    Class NeuralNetwork: defines a neural network with one
+    hidden layer performing binary classification.
+    """
+    def __init__(self, nx, nodes):
+        """
+        Constructor for the NeuralNetwork class.
+
+        Args:
+            nx (int): The number of input features.
+            nodes (int): The number of nodes found in the hidden layer.
+
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is less than 1.
+            TypeError: If nodes is not an integer.
+            ValueError: If nodes is less than 1.
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:  
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = 0
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
