8af786e00703d718b3fa0025274216a31fac0414
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-05-18 03:02:11 +0200

    alpha < 0 conditions

--- supervised_learning/classification/22-deep_neural_network.py
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -172,6 +172,8 @@ class DeepNeuralNetwork:
             raise ValueError("iterations must be a positive integer")
         if type(alpha) is not float:
             raise ValueError("alpha must be a float")
+        if alpha < 0:
+            raise ValueError("alpha must be positive")
 
         for i in range(iterations):
             A, cache = self.forward_prop(X)
