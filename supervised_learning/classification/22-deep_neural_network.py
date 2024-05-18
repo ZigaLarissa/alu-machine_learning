76921ae76406ebd90d80ffa84e4f73c9e6f4bc1b
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-05-18 03:06:26 +0200

    debugg

--- supervised_learning/classification/22-deep_neural_network.py
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -168,7 +168,7 @@ class DeepNeuralNetwork:
         """
         if type(iterations) is not int:
             raise TypeError("iterations must be an integer")
-        if iterations <= 0:
+        if iterations < 1:
             raise ValueError("iterations must be a positive integer")
         if type(alpha) is not float:
             raise ValueError("alpha must be a float")
