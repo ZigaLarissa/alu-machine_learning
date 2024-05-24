56e5cbe7ab555a1f9610a8b0605ab9a917f2a461
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-05-24 06:30:27 +0200

    0th iteration

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -11,6 +11,7 @@ calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
 create_train_op = __import__('5-create_train_op').create_train_op
 
+
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
           alpha, iterations, save_path="/tmp/model.ckpt"):
     """
@@ -39,10 +40,8 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
     # Forward propagation.
     y_pred = forward_prop(x, layer_sizes, activations)
 
-    # Loss.
+    # Loss and accuracy.
     loss = calculate_loss(y, y_pred)
-
-    # Accuracy.
     accuracy = calculate_accuracy(y, y_pred)
 
     # Training operation.
@@ -72,8 +71,8 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
             # Train the network.
             sess.run(train_op, feed_dict={x: X_train, y: Y_train})
 
-            # Print the training accuracy.
-            if i % 100 == 0 or i == iterations:
+            # Print the metrics.
+            if i % 100 == 0 or i == 0 or i == iterations:
                 train_cost = sess.run(loss, feed_dict={x: X_train, y: Y_train})
                 train_accuracy = sess.run(
                     accuracy, feed_dict={x: X_train, y: Y_train}
