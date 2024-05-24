f40484f6106df1045dcd1b45c0704380fe563a0c
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-05-24 07:30:12 +0200

    pycodestyle

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -26,8 +26,12 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
         sess.run(tf.global_variables_initializer())
         for i in range(iterations + 1):
             loss_train = sess.run(loss, feed_dict={x: X_train, y: Y_train})
-            accuracy_train = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
-            loss_valid = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
+            accuracy_train = sess.run(
+                accuracy, feed_dict={x: X_train, y: Y_train}
+                )
+            loss_valid = sess.run(
+                loss, feed_dict={x: X_valid, y: Y_valid}
+                )
             accuracy_valid = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
             if i % 100 == 0 or i == iterations or i == 0:
                 print("After {} iterations:".format(i))
@@ -40,4 +44,4 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
 
         save_path = saver.save(sess, save_path)
 
-        return save_path
+    return save_path
