ce7f3901989e458252685d6ea22ef452040ee456
Ziga Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-05-29 12:54:56 +0200

    pycodestyle validation

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -17,7 +17,7 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     n -- number of nodes the new layer should contain
     activation -- activation function that should be used on the layer
     lambtha -- L2 regularization parameter
-    
+
     Returns:
     The output of the new layer
     """
