9e2ce3071f3cbf9ba19d7f7811157346f9908707
Ziga Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-05-30 06:54:22 +0200

    gropout layer

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -9,28 +9,33 @@ import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob):
-   """
-   Creates a new layer in a neural network with dropout regularization.
+    """
+        Creates a new layer in a neural network with dropout regularization.
 
-   Args:
-       prev (tf.Tensor): Output tensor from the previous layer.
-       n (int): Number of nodes in the new layer.
-       activation (callable): Activation function to apply to the new layer.
-       keep_prob (float): Probability of keeping a node during dropout.
+        Args:
+            prev (tf.Tensor): Output tensor from the previous layer.
+            n (int): Number of nodes in the new layer.
+            activation (callable): Activation function to apply to the new layer.
+            keep_prob (float): Probability of keeping a node during dropout.
 
-   Returns:
-       tf.Tensor: Output tensor of the new layer.
-   """
-   # Initialize weights and biases
-   init = tf.contrib.layers.xavier_initializer()
-   weights = tf.Variable(init(shape=[prev.get_shape().as_list()[-1], n]))
-   biases = tf.Variable(tf.zeros([n]))
-
-   # Apply dropout to the previous layer's output
-   dropout = tf.nn.dropout(prev, keep_prob)
-
-   # Compute the output of the new layer
-   layer = tf.matmul(dropout, weights) + biases
-   layer = activation(layer)
-
-   return layer
+        Returns:
+            tf.Tensor: Output tensor of the new layer.
+    """
+    # Initialize weights and biases
+    init = tf.contrib.layers.variance_scaling_initializer(
+      mode="FAN_AVG"
+    )
+    weights = tf.Variable(
+        init([int(prev.get_shape()[1]), n]),
+        name="weights"
+    )
+    biases = tf.Variable(
+        tf.zeros(n),
+        name="biases"
+    )
+    # Dropout layer
+    dropout = tf.nn.dropout(prev, keep_prob)
+    # Linear combination
+    z = tf.matmul(dropout, weights) + biases
+    # Activation function
+    return activation(z)
