aaae77d7d9e80fdcb3bd3c3c6eaa5d9d29e859a9
ZigaLarissa <ziga0902larissa@gmail.com>
2024-06-05 12:20:23 +0200

    mini batch

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -26,3 +26,34 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
     Returns:
         str: Path where the model was saved.
     """
+    with tf.Session() as sess:
+        sever = tf.train.import_meta_graph(load_path + '.meta')
+        sever.restore(sess, load_path)
+
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        loss = tf.get_collection('loss')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        train_op = tf.get_collection("train_op")[0]
+
+        m = X_train.shape[0]
+        if m % batch_size == 0:
+            num_batches = m // batch_size
+        else:
+            num_batches = m // batch_size + 1
+
+        for epoch in range(epochs + 1):
+            X_shuffled, Y_shuffled = shuffle_data(X_train, Y_train)
+
+            loss_train = sess.run(loss, feed_dict={x: X_train, y: Y_train})
+            acc_train = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
+            loss_valid = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
+            acc_valid = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
+
+            print("After {} epochs:".format(epoch))
+            print("\tTraining Cost: {}".format(loss_train))
+            print("\tTraining Accuracy: {}".format(acc_train))
+            print("\tValidation Cost: {}".format(loss_valid))
+
+        save = sever.save(sess, save_path)
+    return save
