a28fe952afae2487020728350799c2d810ebf0ce
ZigaLarissa <ziga0902larissa@gmail.com>
2024-06-06 07:51:40 +0200

    pycodestyle validation

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -20,6 +20,6 @@ def create_RMSProp_op(loss, alpha, beta2, epsilon):
     returns:
         the RMSProp optimization operation
     """
-    optimizer = tf.train.RMSPropOptimizer(learning_rate = alpha, decay = beta2, epsilon = epsilon)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
     train_op = optimizer.minimize(loss)
     return train_op
