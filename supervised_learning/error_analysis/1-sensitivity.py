c3ce0c032ba17cfda97c56b56021976ac886a20e
ZigaLarissa <ziga0902larissa@gmail.com>
2024-06-13 12:54:47 +0200

    pycodestyle

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -7,13 +7,15 @@ import numpy as np
 
 def sensitivity(confusion):
     """
-    Function that calculates the sensitivity for each class in a confusion matrix
+    Function that calculates the sensitivity for each class in
+    a confusion matrix
     Arguments:
         - confusion is a confusion numpy.ndarray of shape (classes, classes)
             where row indices represent the correct labels and column indices
             represent the predicted labels
             * classes is the number of classes
     Returns:
-        A numpy.ndarray of shape (classes,) containing the sensitivity of each class
+        A numpy.ndarray of shape (classes,) containing the sensitivity
+        of each class
     """
     return np.diag(confusion) / np.sum(confusion, axis=1)
