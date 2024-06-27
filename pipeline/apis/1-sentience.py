54045bfb2df7148ce46e6a74564be1a2dfb4358e
ZigaLarissa <ziga0902larissa@gmail.com>
2024-06-27 09:09:33 +0200

    SENTIENT

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -12,15 +12,23 @@ def sentientPlanets():
     planets of all sentient species.
     """
     url = "https://swapi-api.alx-tools.com/api/species/"
-    planets = []
+    sentient_planets = []
+    sentient_classifications = ["sentient"]
+
     while url:
         response = requests.get(url)
         data = response.json()
-        for species in data['results']:
-            if species["classification"] == "sentient":
-                if species["homeworld"]:
-                    response = requests.get(species["homeworld"])
-                    planet = response.json()
-                    planets.append(planet["name"])
+
+        for species in data["results"]:
+            if (
+                species["classification"].lower() in sentient_classifications
+                and species["homeworld"]
+            ):
+                homeworld_url = species["homeworld"]
+                homeworld_response = requests.get(homeworld_url)
+                homeworld_data = homeworld_response.json()
+                sentient_planets.append(homeworld_data["name"])
+
         url = data["next"]
-    return planets
+
+    return sentient_planets
---
+++ pipeline/apis/2-user_location.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+"""
+By using the Swapi API, create a method that returns
+"""
