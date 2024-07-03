54ba307d2efa3736ff0d2f307a0cf9635d3096f7
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-04 00:23:26 +0200

    column: rating

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -12,4 +12,4 @@ JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
-ORDER BY rating sum DESC;
+ORDER BY rating DESC;
