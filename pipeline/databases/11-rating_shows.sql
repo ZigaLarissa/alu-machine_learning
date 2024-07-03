30ab92771ffef19ab5b701c6bda43396633dac1f
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-03 23:56:52 +0200

    GROUP BY

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -9,4 +9,5 @@
 SELECT tv_shows.title, tv_show_ratings.rate AS rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
 ORDER BY tv_show_ratings.rate DESC;
