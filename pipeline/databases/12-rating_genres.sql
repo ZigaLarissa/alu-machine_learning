59e599b6c684f8f3e7b2977b56dc94384c934257
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-04 00:16:33 +0200

    just rating

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -6,7 +6,7 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating sum
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
