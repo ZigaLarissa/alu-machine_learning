6fa731ee42041d569835e00ecb51164d524c2391
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-03 23:53:09 +0200

    ratings

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -8,5 +8,5 @@
 
 SELECT tv_shows.title, tv_show_ratings.rate
 FROM tv_shows
-JOIN tv_show_rating ON tv_shows.id = tv_show_rating.show_id
-ORDER BY tv_show_rating.rate DESC;
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+ORDER BY tv_show_ratings.rate DESC;
