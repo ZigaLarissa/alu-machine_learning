941353ff50a1eed66fdf1d76f1a5e01f4c1cb599
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-03 22:55:10 +0200

    group

---
+++ pipeline/databases/6-avg_temperatures.sql
@@ -0,0 +1,8 @@
+-- Import in hbtn_0c_0 database a table dump
+-- Write a script that displays the average temperature (Fahrenheit)
+-- by city ordered by temperature (descending).
+
+SELECT city, AVG(temperature) AS average_temperature
+FROM weather
+GROUP BY city
+ORDER BY average_temperature DESC;
