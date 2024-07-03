a92bc84110abaf452253f634cc5364795c093581
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-03 23:01:09 +0200

    avg_temp

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -2,7 +2,7 @@
 -- Write a script that displays the average temperature (Fahrenheit)
 -- by city ordered by temperature (descending).
 
-SELECT city, AVG(value) AS average_temperature
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY average_temperature DESC;
