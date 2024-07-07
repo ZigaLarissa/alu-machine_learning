431b18639559558a29847d84541117246e1c1539
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-07 20:46:28 +0200

    no average score included

--- pipeline/databases/19-bonus.sql
+++ pipeline/databases/19-bonus.sql
@@ -16,9 +16,7 @@ CREATE PROCEDURE AddBonus (
     IN score INT
 )
 BEGIN
-    DECLARE project_id INT DEFAULT NULL;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET project_id = NULL;
-
+    DECLARE project_id INT;
 
     -- Check if the project exists
     SELECT id INTO project_id
@@ -28,22 +26,13 @@ BEGIN
     -- If the project does not exist, create it
     IF project_id IS NULL THEN
         INSERT INTO projects (name)
-        VALUES(project_name);
+        VALUES (project_name);
         SET project_id = LAST_INSERT_ID();
     END IF;
 
     -- Add the correction
     INSERT INTO corrections(user_id, project_id, score)
     VALUES (user_id, project_id, score);
-
-    -- Update the average score of the user
-    UPDATE users
-    SET average_score = (
-        SELECT AVG(score)
-        FROM corrections
-        WHERE user_id = users.id
-    )
-    WHERE id = user_id;
 
 END //
 
