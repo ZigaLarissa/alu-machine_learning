7405c9fa73d72910c89e58d7411764181cb5156a
ZigaLarissa <ziga0902larissa@gmail.com>
2024-09-07 22:30:32 +0200

    typo

--- supervised_learning/neural_style_transfer/3-neural_style.py
+++ supervised_learning/neural_style_transfer/3-neural_style.py
@@ -64,7 +64,7 @@ class NST:
         self.beta = beta
 
         self.load_model()
-        self.generated_features()
+        self.generate_features()
 
     @staticmethod
     def scale_image(image):
