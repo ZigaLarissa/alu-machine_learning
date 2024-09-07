8eb351a20cc81a350e4c2b398d5587fc4472c801
ZigaLarissa <ziga0902larissa@gmail.com>
2024-09-07 23:25:51 +0200

    debug

--- supervised_learning/neural_style_transfer/5-neural_style.py
+++ supervised_learning/neural_style_transfer/5-neural_style.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
 Defines class NST that performs tasks for neural style transfer
--- This file is executable
 """
+
 
 import numpy as np
 import tensorflow as tf
@@ -12,9 +12,24 @@ class NST:
     """
     Performs tasks for Neural Style Transfer
 
-    includes:
-        - constructor
-        - scale_image
+    public class attributes:
+        style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
+                        'block4_conv1', 'block5_conv1']
+        content_layer = 'block5_conv2'
+
+    instance attributes:
+        style_image: preprocessed style image
+        content_image: preprocessed style image
+        alpha: weight for content cost
+        beta: weight for style cost
+
+    class constructor:
+        def __init__(self, style_image, content_image, alpha=1e4, beta=1)
+
+    static methods:
+        def scale_image(image):
+            rescales an image so the pixel values are between 0 and 1
+                and the largest side is 512 pixels
     """
     style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
                     'block4_conv1', 'block5_conv1']
