58b0ad1304ca659ae09ef69ab6685771145f9df4
ZigaLarissa <ziga0902larissa@gmail.com>
2024-09-26 15:06:06 +0200

    changes

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -24,37 +24,15 @@ def tf_idf(sentences, vocab=None):
             word for sentence in tokenized_sentences for word in sentence
             ]
         vocab = sorted(set(all_words))
-    else:
-        all_words = [
-            word for sentence in tokenized_sentences for word in sentence
-            ]
 
-    # Create features list
-    features = [word for word in vocab if word != '' and word != 's']
+    embeddings = []
+    for sentence in tokenized_sentences:
+        sentence_embedding = []
+        for word in vocab:
+            tf = sentence.count(word) / len(sentence) if sentence else 0
+            df = sum([1 for s in tokenized_sentences if word in s])
+            idf = log(len(sentences) / (df + 1))
+            sentence_embedding.append(tf * idf)
+        embeddings.append(sentence_embedding)
 
-    # Create word-to-index mapping
-    word_to_index = {word: index for index, word in enumerate(features)}
-
-    # Calculate document frequency
-    doc_frequency = Counter(
-        word for sentence in tokenized_sentences for word in set(sentence)
-        )
-
-    # Calculate IDF
-    idf = {word: log(
-        len(sentences) / (doc_frequency[word] +1)
-        ) for word in features}
-
-    # Initialize embeddings matrix
-    embeddings = np.zeros((len(sentences), len(features)))
-
-    # Fill embeddings matrix
-    for i, sentence in enumerate(tokenized_sentences):
-        word_counts = Counter(sentence)
-        for word, count in word_counts.items():
-            if word in word_to_index:
-                tf = count / len(sentence)
-                tfidf = tf * idf[word]
-                embeddings[i, word_to_index[word]] = tfidf
-
-    return embeddings, features
+    return np.array(embeddings), vocab
