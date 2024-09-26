57b5245b0925b18ebeab9db348e1e4b1268509c8
ZigaLarissa <ziga0902larissa@gmail.com>
2024-09-26 07:35:14 +0200

    bow

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
+"""
+Bag Of Words
+"""
 
 import numpy as np
-import re
 
 
 def bag_of_words(sentences, vocab=None):
-    word_sentences = [word for word in sentences if word.isalpha()]
-
-    # Tokenize and clean the sentences
-    tokenized_sentences = [re.findall(r'\b\w+\b', sentence.lower()) for sentence in word_sentences]
-    
-    
-    # Build vocabulary if not provided
+    '''
+    Function that creates
+    a bag of words embedding matrix
+    '''
     if vocab is None:
-        vocab = sorted(set(word for sentence in tokenized_sentences for word in sentence))
-    
-    # Create a list to store embeddings
-    embeddings = []
-    
-    # Build the embedding matrix
-    for sentence in tokenized_sentences:
-        # Create a list for the current sentence embedding
-        sentence_embedding = [sentence.count(word) for word in vocab]
-        embeddings.append(sentence_embedding)
-    
-    # Convert embeddings to numpy array
-    embeddings = np.array(embeddings)
-    
+        vocab = set()
+    for sentence in sentences:
+        words = sentence.split()
+        for word in words:
+            vocab.add(word)
+    vocab = sorted(list(vocab))
+    embeddings = np.zeros((len(sentences), len(vocab)))
+    for i, sentence in enumerate(sentences):
+        words = sentence.split()
+        for j, word in enumerate(vocab):
+            embeddings[i, j] = words.count(word)
+
     return embeddings, vocab
