cace98fad405da2550856deaa96c619020fd2f68
ZigaLarissa <ziga0902larissa@gmail.com>
2024-10-09 11:30:21 +0200

    decoder

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -1,63 +1,80 @@
 #!/usr/bin/env python3
 """
-Attention is all we need.
+A class that inherits from tensorflow.keras.layers.Layer
+to decode for machine translation
 """
+
 import tensorflow as tf
 SelfAttention = __import__('1-self_attention').SelfAttention
 
+
 class RNNDecoder(tf.keras.layers.Layer):
     """
-    RNN Decoder
+    Decodes for machine translation:
     """
+
     def __init__(self, vocab, embedding, units, batch):
         """
-        initialized.
+        Class constructor
+        Args:
+            vocab: an integer representing the size of the output vocabulary
+            embedding: an integer representing the dimensionality
+            of the embedding vector
+            units: an integer representing the number of hidden
+            units in the RNN cell
+            batch: an integer representing the batch size
+        Sets the following public instance attributes:
+            embedding: the embedding layer for the targets
+            gru: a GRU layer with units units
+            F: a Dense layer with vocab units
         """
+        if type(vocab) is not int:
+            raise TypeError(
+                "vocab must be int representing the size of output vocabulary"
+            )
+        if type(embedding) is not int:
+            raise TypeError(
+                "embedding must be int representing dimensionality of vector"
+            )
+        if type(units) is not int:
+            raise TypeError(
+                "units must be int representing the number of hidden units"
+            )
+        if type(batch) is not int:
+            raise TypeError("batch must be int representing the batch size")
         super(RNNDecoder, self).__init__()
-        self.units = units
-        self.batch = batch
-
-        # Embedding layer to convert words into embedding vectors
-        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
-
-        # GRU layer for decoding
-        self.gru = tf.keras.layers.GRU(self.units,
-                                       return_sequences=True,
-                                       return_state=True,
-                                       recurrent_initializer='glorot_uniform')
-
-        # Dense layer to produce logits over the target vocabulary
-        self.F = tf.keras.layers.Dense(vocab)
-
-        # Attention mechanism to get the context vector
-        self.attention = SelfAttention(units)
+        self.embedding = tf.keras.layers.Embedding(
+            input_dim=vocab, output_dim=embedding
+        )
+        self.gru = tf.keras.layers.GRU(
+            units=units,
+            return_state=True,
+            return_sequences=True,
+            recurrent_initializer="glorot_uniform",
+        )
+        self.F = tf.keras.layers.Dense(units=vocab)
 
     def call(self, x, s_prev, hidden_states):
         """
-        x: Previous word in the target sequence, shape (batch, 1)
-        s_prev: Previous decoder hidden state, shape (batch, units)
-        hidden_states: Encoder hidden states, shape
-        (batch, input_seq_len, units)
-        Returns:
-          - y: Output word as a one-hot vector in the target
-           vocabulary, shape (batch, vocab)
-          - s: New decoder hidden state, shape (batch, units)
+        x: a tensor of shape (batch, input_seq_len, embedding)
+        containing the embedded input
+        s_prev: a tensor of shape (batch, units) containing
+        the previous decoder hidden state
+        hidden_states: a tensor of shape (batch, input_seq_len, units)
+        containing the outputs of the decoder
+        Returns: y, s_next
+            y: a tensor of shape (batch, target_seq_len, vocab)
+            containing the generated sequences
+            s_next: a tensor of shape (batch, units) containing
+            the next decoder hidden state
         """
-        # Get the context vector from the attention mechanism
-        context, _ = self.attention(s_prev, hidden_states)
-
-        # Embed the previous word (x) in the target sequence
-        x = self.embedding(x)  # Shape: (batch, 1, embedding)
-
-        # Concatenate context vector with embedded input (x)
-        x = tf.concat([tf.expand_dims(context, 1), x], axis=-1)
-
-        # Pass concatenated input and previous state through the GRU
-        output, s = self.gru(x, initial_state=s_prev)
-
-        # Reshape output and pass through the dense layer
-        # to get vocab-size logits
-        output = tf.reshape(output, (-1, output.shape[2]))
-        y = self.F(output)  # Shape: (batch, vocab)
-
+        units = s_prev.get_shape().as_list()[1]
+        attention = SelfAttention(units)
+        context, weights = attention(s_prev, hidden_states)
+        x = self.embedding(x)
+        context = tf.expand_dims(context, 1)
+        x = tf.concat([context, x], axis=-1)
+        y, s = self.gru(x)
+        y = tf.reshape(y, (-1, y.shape[2]))
+        y = self.F(y)
         return y, s
