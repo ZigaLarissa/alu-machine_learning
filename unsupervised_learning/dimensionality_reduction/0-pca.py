7a7e3d9492fead874976284f073662f85ebff017
ZigaLarissa <ziga0902larissa@gmail.com>
2024-10-19 22:57:17 +0200

    pca

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,39 +1,32 @@
 #!/usr/bin/env python3
-"""
-PCA
-"""
+"""Write a function that performs PCA on dataset"""
+
 
 import numpy as np
 
 
 def pca(X, var=0.95):
     """
-    Function that performs PCA on a dataset
+    PCA on a dataset
+
+    parameters:
+        X [numpy.ndarray of shape (n, d)]: dataset
+            n: number of data points
+            d: number of dimensions in each data point
+            all dimensions have a mean of 0 across all data points
+        var [float]: the fraction of the variance that the PCA
+            transformation should maintain
+
+    returns:
+        W [numpy.ndarray of shape (d, nd)]: the weights matrix that
+            maintains var fraction of X's original variance
+            d: number of dimensions of each data point
+            nd: new dimensionality of the transformed X
     """
-    # Normalize the mean value
-    X_mean = X - np.mean(X, axis=0)
-
-    # create the covariance matrix
-    cov = np.cov(X_mean.T)
-
-    # Eigenvalues and eigenvectors
-    w, v = np.linalg.eig(cov)
-
-    # Sort eigenvalues
-    idx = np.sort(w)[::-1]
-    w_sort = w[idx]
-    v_sort = v[:, idx]
-
-    # Explained variance
-    exp_var = w_sort / np.sum(w_sort)
-
-    # Cumulative explained variance
-    cum_exp_var = np.cumsum(exp_var)
-
-    # Number of dimensions to keep
-    d = np.argwhere(cum_exp_var >= var)[0, 0]
-
-    # Projection matrix
-    W = v_sort[:, :d + 1]
-
-    return W
+    # n, d = X.shape
+    u, s, v = np.linalg.svd(X)
+    ratios = list(x / np.sum(s) for x in s)
+    variance = np.cumsum(ratios)
+    nd = np.argwhere(variance >= var)[0, 0]
+    W = v.T[:, :(nd + 1)]
+    return (W)
